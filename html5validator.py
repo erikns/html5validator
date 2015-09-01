16300e094018d5e5bfea35659dbaab9ebb2f5316
Erik Sørensen <eriksorensen88@gmail.com>
2015-09-01 14:35:36 +0200

    Basic color output

--- html5validator.py
+++ html5validator.py
@@ -3,6 +3,7 @@
 
 import sys
 import requests
+import json
 
 
 VERSION = '1.0.1'
@@ -30,13 +31,39 @@ class Validator:
         with open(self.file) as file_content:
             content = file_content.read()
         r = requests.post(Validator.rest_url, 
-                params={'out': 'gnu'}, 
+                params={'out': 'json'}, 
                 headers={'Content-Type': 'text/html; charset=UTF-8'},
                 data=content)
         if r.status_code == 200:
             return r.text
         else:
             return ''
+
+
+class ValidationOutputFormatter:
+    def __init__(self):
+        pass
+
+    def output_stdout(self, validation):
+        validation_json = json.loads(validation)
+        # print validation_json
+
+        messages = validation_json['messages']
+        for message in messages:
+            if message['type'] == 'info':
+                print bcolors.OKGREEN + 'Info:\t' + message['message'] + bcolors.ENDC
+            elif message['type'] == 'warning':
+                print bcolors.WARNING + 'Warning:\t' + message['message'] + bcolors.ENDC
+            elif message['type'] == 'error':
+                print bcolors.FAIL + 'Error:\t' + message['message'] + bcolors.ENDC
+
+    def output_stdout_old(self, validation):
+        if len(validation) > 0:
+            print bcolors.FAIL + 'Found something:' + bcolors.ENDC
+            print validation
+        else:
+            print bcolors.OKGREEN + 'No errors or warnings found' + bcolors.ENDC
+        print 'Validation complete'
 
 
 def main(argv):
@@ -57,12 +84,9 @@ def do_validation(input_file):
     v = Validator(input_file)
     print 'Validating ' + input_file + '...'
     validation = v.validate()
-    if len(validation) > 0:
-        print bcolors.FAIL + 'Found something:' + bcolors.ENDC
-        print validation
-    else:
-        print bcolors.OKGREEN + 'No errors or warnings found' + bcolors.ENDC
-    print 'Validation complete'
+    output = ValidationOutputFormatter()
+    # output.output_stdout_old(validation)
+    output.output_stdout(validation)
 
 
 def version():
