5e7fe456dad3623d5e187f9aaf97f42300b30bf0
Erik Sørensen <eriksorensen88@gmail.com>
2016-08-02 22:57:33 +0200

    Updated copyright to 2016

--- html5validator.py
+++ html5validator.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 #  html5validator
-#  Copyright (C) 2015  Erik Sørensen
+#  Copyright (C) 2016  Erik Sørensen
 #
 #  This program is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
